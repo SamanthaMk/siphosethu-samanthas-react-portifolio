bab6417630f8b5499f9b9a4d1c7438c8cd3b00ee
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2024-01-05 16:46:03 +0200

    I updated my about section i added a coding video and a paragraph that entails more about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./about.css";
+import Me from "../../assets/coding-video.mp4";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+<div className="ccontainer about__container">
+<div className="about__me">
+<video src={Me} autoPlay loop muted className="vid"></video>
+</div>
+
+<div className="about__content">
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+  Ipsam adipisci, ab nam ut molestias nihil qui hic molestiae
+  perspiciatis voluptatibus doloremque amet et sapiente vel
+  animi odio dolore laborum voluptates Lorem ipsum dolor sit 
+  amet consectetur adipisicing elit. Quaerat vel ea sit unde
+  aperiam cum nemo veritatis quisquam vitae recusandae eos
+  aliquid repellendus quo, laboriosam magni ipsam
+   laborum blanditiis quam.
+</p>
+<a href="#contact" className="btn btn-primary" >Lets Chat</a>
+</div>
+
+</div>
+    </section>
+
+  );
+
 };
 
 export default About;

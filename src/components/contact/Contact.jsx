55d591ce0759dedb8691f76cd9a4c25f6a1c44ee
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2024-01-14 16:39:28 +0200

    I updated my contact component, I added my contacts , my social media accounts and i added a form which i linked with email js  so that potential clients or   recruiters can send me an email directly on in my website

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,101 @@
-import React from 'react'
-import './contact.css'
+import React from "react";
+import "./contact.css";
+import { MdOutlineMail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa6";
+import { BsWhatsapp } from "react-icons/bs";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
-}
+  const form = useRef();
 
-export default Contact
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_a56pfmv",
+        "template_9otth2h",
+        form.current,
+        "qpPMXKOOJzG2hg16V"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>samanthamkandwana17@gmail.com</h5>
+            <a
+              href="mailto:samanthamkandwana17@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send A Message
+            </a>
+          </article>
+          <article className="contact__option">
+            <FaLinkedin className="contact__option-icon" />
+            <h4>Linkedin</h4>
+            <h5>Siphosethu Mkandwana</h5>
+            <a
+              href="www.linkedin.com/in/siphosethu-mkandwana-a80018279"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send A Message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>Text Me</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+27655070664"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Send A Message
+            </a>
+          </article>
+        </div>
+        {/* END of contact options*/}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/testimonials/Testimonials.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './testimonials.css'
-
-const Testimonials = () => {
-  return <section id="testimonals">Testimonials</section>;
-};
-
-export default Testimonials

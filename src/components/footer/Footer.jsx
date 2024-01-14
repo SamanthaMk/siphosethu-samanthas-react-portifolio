190af48a81c66a3ed45ae937dfeb29e8e60e70b6
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2024-01-14 18:51:13 +0200

    I  updated the footer i added styling and social media links and I made it responsive on phones and tablets

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="/#" className="footer__logo">
+        SSM
+      </a>
+      <ul className="premalinks">
+        <li>
+          <a href="/#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
 
-export default Footer
+      <div className="footer__socials">
+        <a href="https://facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://instagram.com">
+          <FaInstagram />
+        </a>
+        <a href="https://twitter.com">
+          <FaXTwitter />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy;Siphosethu Samantha All rights Reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

553ceba0d655bca4029bc6ffee7207b352aa2028
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2024-03-16 09:14:23 +0200

    I deleted my previous CV and replaced it with my newly updated current resume`

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import CV from  '../../assets/fake-cv.pdf'
+import React from "react";
+import CV from "../../assets/CV-of-Siphosethu-Samantha-Mk.pdf";
 
 const CTA = () => {
   return (
-    <div className='cta'> 
-    <a href={CV} download className='btn btn-primary'> Download CV</a>
-    <a href="#contact" className='btn btn-primary'>Lets Connect</a>
+    <div className="cta">
+      <a href={CV} download className="btn btn-primary">
+        {" "}
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        Lets Connect
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;

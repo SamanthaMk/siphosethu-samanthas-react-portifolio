dc32dea807e298da251222dcee28a3f6a091201d
Siphosethu Samantha Mkandwana <132946841+SamanthaMk@users.noreply.github.com>
2024-04-25 17:01:28 +0200

    I  updated my portifolio I adding another project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,32 +1,44 @@
 import React from "react";
 import "./projects.css";
-import IMG1 from "../../assets/movie-m-h.png";
-import IMG2 from "../../assets/zoom-car-hire.png";
-import IMG3 from "../../assets/nikey.png";
+import IMG1 from "../../assets/fanta.png";
+import IMG2 from "../../assets/movie-m-h.png";
+import IMG3 from "../../assets/zoom-car-hire.png";
+import IMG4 from "../../assets/nikey.png";
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: "Movie Rush APP",
-    info: "Project completed using Html and CSS ,Repository found on Github and  Deployed using Vercel.",
-    github: "https://github.com/SamanthaMk/movie-rush",
-    demo: "https://movie-rush-henna.vercel.app/",
-    paragraph:"Welcome to Movie Rush, where every movie night is an adventure! Explore a curated selection of blockbuster hits, timeless classics, and hidden gems. With personalized recommendations and easy navigation, finding your next favorite film is a thrill in itself. Join us and elevate your movie-watching experience to new heights!"
+    title: "Fanta Explosion of Flavours App",
+    info: "Project completed using Html and CSS ,Repository found on Github, Mobile responsive and  Deployed using Vercel.",
+    github: "https://github.com/SamanthaMk/fanta-explosion",
+    demo: "https://fanta-explosion.vercel.app/",
+    paragraph:
+      "Fanta, a brand under Coca-Cola, originated during World War II in Germany due to ingredient shortages.Its name reflects imagination Fantasie in German.Since then, it has grown into a globally recognized soda with a range of fruity flavors. Fanta's success is attributed to its playful marketing, diverse flavor offerings, and ability to connect with younger consumers through digital platforms.",
   },
   {
     id: 2,
     image: IMG2,
+    title: "Movie Rush APP",
+    info: "Project completed using Html and CSS ,Repository found on Github,  Mobile responsive and  Deployed using Vercel.",
+    github: "https://github.com/SamanthaMk/movie-rush",
+    demo: "https://movie-rush-henna.vercel.app/",
+    paragraph:
+      "Welcome to Movie Rush, where every movie night is an adventure! Explore a curated selection of blockbuster hits, timeless classics, and hidden gems. With personalized recommendations and easy navigation, finding your next favorite film is a thrill in itself. Join us and elevate your movie-watching experience to new heights!",
+  },
+  {
+    id: 3,
+    image: IMG3,
     title: "Zoom Fleet Car Rentals",
-    info: "Project completed using Javascript React,TypeScript, Next.js, Tailwind CSS, Headless UI, with an Api and Deployed using Vercel.",
+    info: "Project completed using Javascript React,TypeScript, with SEO Optimization ,Next.js, Tailwind CSS, Headless UI, with an Api and Deployed using Vercel.",
     github: "https://github.com/SamanthaMk/zoom_fleet_rentals",
     demo: "https://zoom-fleet-rentals.vercel.app/",
     paragraph:
       "Welcome to Zoom Fleet Rentals, the ultimate car rental destination offering a perfect blend of luxury and affordability Our world-class fleet caters to diverse tastes and budgets, ensuring an unforgettable travel experience.With seamless bookings, a user-friendly app, and top-notch customer service, we proudly stand as the best car rental business globally. Elevate your journey with Zoom Fleet Rentals where every drive is an adventure!",
-  },  
+  },
   {
-    id: 3,
-    image: IMG3,
+    id: 4,
+    image: IMG4,
     title: "NIKE cloned App",
     info: "Project completed using Javascript React, Vite & Tailwind CSS and is mobile responsive,Repository found on Github and  Deployed using Vercel.",
     github: "https://github.com/SamanthaMk/nike",
